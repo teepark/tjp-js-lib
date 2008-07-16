fec308b04765744a4ef03857e03741f04ebab9ac
Travis J Parker <travis.parker@gmail.com>
2008-07-15 21:55:02 -0700

    initial import

---
+++ cookie.js
@@ -0,0 +1,82 @@
+/*
+cookie - handy utilities for browser cookie CRUD
+
+cookie.get(name)
+  returns the value stored under name
+
+cookie.getall()
+  returns an object whose key/value pairs are all the cookies available
+
+cookie.set(name, value, options)
+  stores value under name in the browser cookies
+  options is an optional object whose key/value pairs customize the storage:
+    - path: the url-path under which the cookie is accessible (default "/")
+    - domain: the domain for which the cookie is accessible (defaults to
+        anything under top-level host, e.g. www.example.com -> .example.com)
+    - max-age: the number of seconds for which the cookie will last (defaults
+        to null, meaning browser session)
+    - expires: another way of specifying max-age, but max-age takes precedence
+        and if expires is used, value will be converted to a max-age. this must
+        be a GMTString (default null)
+    - secure: if true, cookie is only available to web-pages under https 
+        (default false)
+
+cookie.remove(name)
+  removes the cookie with name (stores with max-age of 0)
+*/
+(function(tjp) {
+  tjp.cookie.get = function(name) {
+    var i, pairs, pair, result = {};
+    pairs = document.cookie.split(";");
+    for (i = 0; i < pairs.length; i++) {
+      pair = pairs[i].split("=");
+      if (pair[0] === name) return pair[1];
+    }
+    return null;
+  };
+
+  tjp.cookie.set = function(name, value, options) {
+    var opt, expdate, cookiestr;
+
+    options = tjp.base.extend(tjp.base.extend({}, defaultOptions),
+        options || {});
+
+    expdate = Date.parse(options.expires);
+    if (!(options['max-age'] instanceof Number) && !isNaN(expdate)) {
+      options['max-age'] = Math.round((expdate -
+          (new Date()).getTime()) / 1000);
+    }
+    delete options.expires;
+
+    cookiestr = name + "=" + value;
+    for (opt in options) {
+      if (options[opt] != null && opt !== "secure")
+        cookiestr += "; " + opt + "=" + options[opt];
+    }
+    if (options.secure) cookiestr += "; secure";
+
+    document.cookie = cookiestr;
+  };
+
+  tjp.cookie.remove = function(name) {
+    tjp.cookie.set(name, "", {"max-age": 0});
+  };
+
+  tjp.cookie.getall = function() {
+    var i, pairs, pair, result = {};
+    pairs = document.cookie.split(";");
+    for (i = 0; i < pairs.length; i++) {
+      pair = pairs[i].split("=");
+      result[pair[0]] = pair[1];
+    }
+    return result;
+  };
+
+  var defaultOptions = {
+    path: "/",
+    domain: "." + document.location.hostname.split(".").slice(-2).join("."),
+    expires: -1,
+    secure: false
+  };
+  defaultOptions["max-age"] = null;
+})(com.travisjparker);
---
+++ http.js
@@ -0,0 +1,165 @@
+/*
+http - functions for easy cross-browser HTTP requests
+
+http.get(options)
+  sends a GET request - same options as http.request except method.
+
+http.post(options)
+  sends a POST request - same options as http.request except method.
+
+http.head(options)
+  sends a HEAD request - same options as http.request except method.
+
+http.request(options)
+  sends a request using attributes of 'options' object for configuration:
+    - url is the URL for the request (required)
+    - method is the method such as GET, POST, etc (default "GET")
+    - requestType will be the Content-Type header of the request (default
+        "application/x-www-form-urlencoded")
+    - responseType is the Content-Type to treat the response as, regardless of
+        what is actually sent (default null)
+    - timeout is a maximum amount of time in milliseconds to allow for the
+        request (default 0, which means no timeout)
+    - data is an object whose properties are serialized and sent to the server
+        in the format appropriate for the method (default null)
+    - parseData is a flag for whether or not to automatically serialize
+        options.data (default true)
+    - async is whether or not to handle the response asynchronously (default
+        true)
+    - headers is an object whose properties are sent in the request as HTTP
+        headers (default null)
+    - success is a function to be used if the response succeeds in under the
+        timeout - it will be passed a response object (default null)
+    - failure is a function to be used if the response fails or times out - it
+        will be passed a response object (default null)
+  also returns the XMLHttpRequest object used.
+
+Response objects:
+  callback functions receive a single argument, the response. This is a
+  javascript object with the following properties:
+    status: the status code (e.g. 200 for OK)
+    statusText: the string-representation of the status (e.g. "OK")
+    text: the text of the response
+    xml: if indicated by the Content-Type of the response or an overridden
+      responseType, this is an XML document of the response
+    headers: an object whose name/value pairs are the headers of the response
+*/
+(function(tjp) {
+  tjp.http.get = function(o) {
+    return tjp.http.request(tjp.base.extend(tjp.base.extend({}, o),
+        {method: "GET"}));
+  };
+
+  tjp.http.post = function(o) {
+    return tjp.http.request(tjp.base.extend(tjp.base.extend({}, o),
+        {method: "POST"}));
+  };
+
+  tjp.http.head = function(o) {
+    return tjp.http.request(tjp.base.extend(tjp.base.extend({}, o),
+        {method: "HEAD"}));
+  };
+
+  tjp.http.request = function(o) {
+    var name, xhr;
+
+    xhr = makeXHR();
+
+    o = tjp.base.extend(tjp.base.extend({}, defaultRequestOptions), o);
+    o.method = o.method.toUpperCase();
+
+    if (o.parseData && !(o.data instanceof String))
+      o.data = tjp.base.urlencode(o.data);
+    o.data = o.data.toString();
+    if (o.data && (o.method === "GET" || o.method === "HEAD")) {
+      o.url += "?" + o.data;
+      o.data = null;
+    }
+
+    if (o.headers !== null)
+      for (name in headers) xhr.setRequestHeader(name, headers[name]);
+
+    if (o.url === null) return xhr;
+
+    xhr.open(o.method, o.url, o.async);
+
+    if (o.async) {
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          var
+            response = {
+              status: xhr.status,
+              statusText: xhr.statusText,
+              text: xhr.responseText,
+              xml: xhr.responseXML,
+              headers: parseHeaders(xhr.getAllResponseHeaders())
+            },
+            handler = (xhr.status >= 200 && xhr.status < 300) ?
+              o.success : o.failure;
+          if (handler instanceof Function) handler(response);
+        }
+      };
+    }
+
+    xhr.send(o.data);
+
+    if (!o.async) {
+      var
+        response = {
+          status: xhr.status,
+          statusText: xhr.statusText,
+          text: xhr.responseText,
+          xml: xhr.responseXML,
+          headers: parseHeaders(xhr.getAllResponseHeaders())
+        },
+        handler = (xhr.status >= 200 && xhr.status < 300) ?
+          o.success : o.failure;
+      if (handler instanceof Function) handler(response);
+      return xhr;
+    }
+  };
+
+  var defaultRequestOptions = {
+    url: null,
+    method: "GET",
+    requestType: "application/x-www-form-urlencoded",
+    responseType: null,
+    timeout: 0,
+    data: null,
+    parseData: true,
+    async: true,
+    headers: null,
+    success: null,
+    failure: null
+  };
+
+  var ms_xml_types = [
+    "Msxml6.XMLHTTP",
+    "Msxml5.XMLHTTP",
+    "Msxml4.XMLHTTP",
+    "Msxml3.XMLHTTP",
+    "Msxml2.XMLHTTP",
+    "Microsoft.XMLHTTP"
+  ];
+  function makeXHR() {
+    var i;
+    if (window.ActiveXObject) {
+      for (i = 0; i < ms_xml_types.length; i++) {
+        try { return new ActiveXObject(ms_xml_types[i]); }
+        catch (e) {}
+      }
+    }
+    return window.XMLHttpRequest ? new XMLHttpRequest() : null;
+  };
+
+  function parseHeaders(raw) {
+    var i, lines, pair, result = {};
+    lines = raw.split("\n");
+    for (i = 0; i < lines.length; i++) {
+      pair = lines[0].split(":");
+      if (pair[1].slice(0, 1) === " ") pair[1] = pair[1].slice(1);
+      result[pair[0].toLowerCase()] = pair[1];
+    }
+    return result;
+  };
+})(com.travisjparker);
