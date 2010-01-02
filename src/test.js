99a918e7cb64a25efffe6b62d4001ec1f45aac8c
Travis J Parker <travis.parker@gmail.com>
2010-01-02 04:02:25 -0800

    fully working test and suite, needs some smoothing over the hard edges of the api

---
+++ src/test.js
@@ -0,0 +1,245 @@
+/*
+test - simple asyncronous unit testing for javascript
+*/
+
+/*global TJP*/
+//context:browser
+//context:console
+
+var test = TJP.test = TJP.test || {};
+
+var oneTimer = TJP.event.oneTimer,
+    dispatch = TJP.event.dispatch,
+    clone = TJP.base.clone,
+    extend = TJP.base.extend;
+
+var PASS = test.PASS = 0,
+    FAIL = test.FAIL = 1,
+    ERROR = test.ERROR = 2;
+
+var TestComplete = function(result) {
+  this.result = result;
+  this.name = "TestComplete";
+};
+TestComplete.prototype = new Error();
+
+var scopeExtension = {
+  'pass': function() { throw new TestComplete(PASS); },
+  'fail': function() { throw new TestComplete(FAIL); },
+  'assert': function(truth) { if (!truth) throw new TestComplete(FAIL); }
+};
+
+var TestRun = {
+  'create': function(test) {
+    return clone(this, {'template': test, 'func': test.func, 'done': false});
+	},
+
+  'setUp': function(scope) {
+    var self = this;
+
+    scope = this.scope = clone(scope);
+
+    this.template.setUp.call(scope);
+
+    oneTimer(this, "finished", function(target, type, data) {
+      dispatch(self.template, type, data);
+    });
+
+    extend(scope, scopeExtension);
+	},
+
+  'complete': function(result, error) {
+    if (this.done) return;
+    this.done = true;
+
+    dispatch(this, "finished", {
+      'result': result,
+      'passed': !result,
+      'time': (new Date()).getTime() - this.started,
+      'error': error
+    });
+
+    this.tearDown();
+  },
+
+  'tearDown': function() {
+    this.template.tearDown.call(this.scope);
+
+    delete this.scope;
+	},
+
+  'run': function() {
+    this.started = (new Date()).getTime();
+
+    try {
+      this.func.apply(this.scope, this.arguments);
+    } catch (err) {
+      if (err instanceof TestComplete) {
+        this.complete(err.result);
+     } else {
+        this.complete(ERROR, err);
+      }
+    }
+	}
+};
+
+var SuiteRun = {
+  'create': function(suite) {
+    var i,
+        children = [];
+
+    for (i = 0; i < suite.children.length; i++)
+      children.push(suite.children[i]._makeRun());
+
+    return clone(this, {'template': suite, 'children': children});
+  },
+
+  'setUp': function(scope) {
+    var i,
+        self = this,
+        suiteSetUp = this.template.setUp,
+        child,
+        children = this.children,
+        run = this; // for closures below
+
+    scope = this.scope = clone(scope);
+    this.results = new Array(children.length);
+    this.doneCount = 0;
+
+    if (suiteSetUp) suiteSetUp.call(scope);
+
+    for (i = 0; i < children.length; i++) {
+      child = children[i];
+
+      (function(i) { // freeze i
+        oneTimer(child, "finished", function(target, type, data) {
+          run.results[i] = data;
+          if (++run.doneCount === children.length)
+            run.complete();
+        });
+      })(i);
+
+      child.setUp(scope);
+    }
+
+    oneTimer(this, "finished", function(target, type, data) {
+      dispatch(self.template, type, data);
+    });
+  },
+
+  'complete': function() {
+    var i,
+        foundFail = false,
+        foundError = false,
+        error,
+        result,
+        results = this.results,
+        ended = (new Date()).getTime();
+
+    for (i = 0; i < results.length; i++) {
+      result = results[i];
+      if (result.result === ERROR) {
+        foundError = true;
+        error = result.error;
+        break;
+      } else if (result.result === FAIL) {
+        foundFail = true;
+      }
+    }
+
+    dispatch(this, "finished", {
+      'result': foundError ? ERROR: (foundFail ? FAIL : PASS),
+      'passed': !result,
+      'time': ended - this.started,
+      'error': foundError ? error : undefined,
+      'children': results
+    });
+
+    this.tearDown();
+  },
+
+  'tearDown': function() {
+    var i,
+        suiteTearDown = this.template.tearDown,
+        scope = this.scope,
+        children = this.children;
+
+    for (i = 0; i < children.length; i++)
+      children[i].tearDown();
+
+    if (suiteTearDown) suiteTearDown.call(scope);
+
+    delete this.scope;
+  },
+
+  'run': function() {
+    var i,
+        children = this.children;
+
+    this.started = (new Date()).getTime();
+
+    for (i = 0; i < children.length; i++) {
+      children[i].run();
+    }
+  }
+};
+
+var Runnable = {
+  '_makeRun': function() {
+    return this._runType.create(this);
+  },
+
+  'run': function() {
+    var run,
+        self = this;
+
+    if (!TJP.event)
+      throw new Error("TJP.test requires TJP.event");
+
+    run = this._makeRun();
+    run.setUp({});
+
+    run.run();
+  }
+};
+
+var TestObject = clone(Runnable, {
+  '_runType': TestRun,
+
+  'create': function(func, options) {
+    options = options || {};
+
+    return clone(this, {
+      'func': func,
+      'arguments': options.arguments || [],
+      'setUp': options.setUp || function() {},
+      'tearDown': options.tearDown || function() {}
+    });
+  }
+});
+
+var SuiteObject = clone(Runnable, {
+  '_runType': SuiteRun,
+
+  'create': function(children, options) {
+    options = options || {};
+
+    return clone(this, {
+      'children': (children && children.length) ? children.slice() : [],
+      'setUp': options.setUp || function() {},
+      'tearDown': options.tearDown || function() {}
+    });
+  },
+
+  'addChild': function(child) {
+    this.children.push(child);
+  }
+});
+
+test.test = function(func, options) {
+  return TestObject.create(func, options);
+};
+
+test.suite = function(children, options) {
+  return SuiteObject.create(children, options);
+};
